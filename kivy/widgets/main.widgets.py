a9e7a0fc4eea573c58caa9b74ed1bffc9c740584
danielmansueto <dmasueto30@gmail.com>
2019-04-30 14:11:06 -0500

    finished

---
+++ kivy/widgets/main.widgets.py
@@ -0,0 +1,29 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.properties import ObjectProperty
+
+Window.clearcolor = (0,0,0,1)
+Window.size = (400, 600)
+
+class DemoApp(App):
+    def build(self):
+        return DemoLayout()
+
+class DemoLayout(BoxLayout):
+    def switch_on(self, switch, value):
+        if value:
+            print("switch on")
+            Window.clearcolor = (1, 1, 1, 1)
+            self.switch_text.color = (0,0,0,1)
+        else:
+            print("switch off")
+            Window.clearcolor = (0, 0, 0, 1)
+            self.switch_text.color = (1,1,1,1)
+
+    def slider_change(self, slider, value):
+        self.slider_text.color = (value, value, value, 1)
+
+if __name__ == "__main__":
+    demo = DemoApp()
+    demo.run()
