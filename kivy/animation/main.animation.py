50f243ec55c569552519129d191766a9ee7852b9
danielmansueto <dmasueto30@gmail.com>
2019-05-14 16:42:07 -0500

    finished

---
+++ kivy/animation/main.animation.py
@@ -0,0 +1,46 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.animation import Animation
+Window.size = [600, 600]
+
+class AniApp(App):
+    def build(self):
+        return(AniLayout())
+
+class AniLayout(Widget):
+    def on_touch_down(self, touch):
+        print(touch.x, touch.y)
+        #self.ani_rect.center_x = touch.x
+        #self.ani_rect.center_y = touch.y
+        self.ani_rect.animated_move(touch.x, touch.y)
+
+class ClockRect(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Clock.schedule_interval(self.update, 1/60)
+        self.speedx = 5
+        self.speedy = 3
+
+    def update(self, *args):
+        self.x += self.speedx
+        if self.right > Window.width or self.x < 0:
+            self.speedx *= -1
+
+        self.y += self.speedy
+        if self.y < 0 or self.top > Window.height:
+            self.speedy *= -1
+
+class AniRect(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def animated_move(self, x, y):
+        animation1 = Animation(center_x=x, center_y=y, duration=1, transition="out_elastic")
+        Animation.cancel_all(self)
+        animation1.start(self)
+
+if __name__ == "__main__":
+    app = AniApp()
+    app.run()
--- kivy/widgets/main.widgets.py
+++ kivy/widgets/main.widgets.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
 
 Window.clearcolor = (0,0,0,1)
 Window.size = (400, 600)
@@ -23,6 +24,16 @@ class DemoLayout(BoxLayout):
 
     def slider_change(self, slider, value):
         self.slider_text.color = (value, value, value, 1)
+
+    def check_box(self, value):
+        print("box checked", value)
+
+    def spinner_clicked(self, value):
+        print(value)
+
+    def popup(self):
+        pop = Popup()
+        pop.open()
 
 if __name__ == "__main__":
     demo = DemoApp()
